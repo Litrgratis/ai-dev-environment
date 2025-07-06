e91916acc4e6abe13502656d585be25fcf247a6f
Litrgratis <litrgratis@gmail.com>
2025-07-06 21:57:31 +0000

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -20,6 +20,10 @@ provider "kubernetes" {
   config_path = var.kubeconfig
 }
 
+// ...add resources and modules here...
+  config_path = var.kubeconfig_path
+}
+
 provider "helm" {
   kubernetes {
     config_path = var.kubeconfig
