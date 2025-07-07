fca39ae7fc58477691a6e6ce83d80d6cdf730bda
Litrgratis <litrgratis@gmail.com>
2025-07-07 18:39:34 +0000

    Aktualizacja: pełna struktura projektu, frontend React, backend, konfiguracje środowiskowe

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Witaj w Twoim AI-Dev-Environment Frontend!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
